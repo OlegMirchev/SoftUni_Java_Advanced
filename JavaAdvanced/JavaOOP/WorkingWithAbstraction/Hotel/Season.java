55e3891826a53bd471c67fb7f084017c16c46366
OlegMirchev <olegmirchev@gmail.com>
2022-09-27 10:11:20 +0300

    Added homework

---
+++ JavaAdvanced/JavaOOP/WorkingWithAbstraction/Hotel/Season.java
@@ -0,0 +1,18 @@
+package JavaAdvanced2021.JavaOPP.WorkingWithAbstractionLAB2610.Hotel;
+
+public enum Season {
+    AUTUMN(1),
+    SPRING(2),
+    WINTER(3),
+    SUMMER(4);
+
+    private int multiplier;
+
+    Season(int multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    public int getMultiplier() {
+        return multiplier;
+    }
+}
