22bfaad19fbe0cb1e535ccb76e25c3179df4b7fa
OlegMirchev <olegmirchev@gmail.com>
2022-10-09 08:52:52 +0300

    Added homework

---
+++ JavaAdvanced/JavaOOP/Inheritance/Person/Child.java
@@ -0,0 +1,8 @@
+package JavaAdvanced2021.JavaOPP.InheritanceExercises0411.Person;
+
+public class Child extends Person {
+
+    public Child(String name, int age) {
+        super(name, age);
+    }
+}
