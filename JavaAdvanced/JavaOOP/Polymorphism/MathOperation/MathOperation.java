0323acf8557b25a92bc860399dd86c0f9cbdfc7b
OlegMirchev <olegmirchev@gmail.com>
2022-10-18 08:06:06 +0300

    Added homework

---
+++ JavaAdvanced/JavaOOP/Polymorphism/MathOperation/MathOperation.java
@@ -0,0 +1,16 @@
+package JavaAdvanced2021.JavaOPP.PolymorphismLAB1011.MathOperation;
+
+public class MathOperation {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+}
---
+++ JavaAdvanced/JavaOOP/Polymorphism/MathOperation/main.java
@@ -0,0 +1,11 @@
+package JavaAdvanced2021.JavaOPP.PolymorphismLAB1011.MathOperation;
+
+public class main {
+    public static void main(String[] args) {
+        MathOperation math = new MathOperation();
+        System.out.println(math.add(2, 2));
+        System.out.println(math.add(3, 3, 3));
+        System.out.println(math.add(4, 4, 4, 4));
+
+    }
+}
