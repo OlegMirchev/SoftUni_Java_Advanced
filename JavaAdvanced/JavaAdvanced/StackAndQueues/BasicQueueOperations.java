91ad8d879ebeaf4b44e78faa76b4500b63402d9b
OlegMirchev <olegmirchev@gmail.com>
2022-10-30 08:09:09 +0200

    Added homework

---
+++ JavaAdvanced/JavaAdvanced/StackAndQueues/BasicQueueOperations.java
@@ -0,0 +1,44 @@
+package JavaAdvanced2021.JavaAdvanced.StacksAndQueuesExercises1609;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BasicQueueOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int s = scanner.nextInt();
+        int x = scanner.nextInt();
+
+        ArrayDeque<Integer> stackNumber = new ArrayDeque<>();
+
+        for (int i = 1; i <= n; i++) {
+            stackNumber.offer(scanner.nextInt());
+        }
+        for (int j = 1; j <= s; j++) {
+            stackNumber.poll();
+        }
+        if (stackNumber.contains(x)) {
+            System.out.println("true");
+        } else {
+            if (!stackNumber.isEmpty()) {
+                System.out.println(getMin(stackNumber));
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+
+    private static int getMin(ArrayDeque<Integer> stackNumber) {
+        int min = Integer.MAX_VALUE;
+        while (!stackNumber.isEmpty()) {
+            int currentNumb = stackNumber.pop();
+
+            if (currentNumb < min) {
+                min = currentNumb;
+            }
+        }
+        return min;
+    }
+}
+
