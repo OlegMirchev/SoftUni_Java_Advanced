30e6a78c5472c865cb5042145132bfa26dc31812
OlegMirchev <olegmirchev@gmail.com>
2022-11-23 08:48:53 +0200

    Added homework

---
+++ JavaAdvanced/JavaAdvanced/Generics/GenericSwapMethodString.java
@@ -0,0 +1,52 @@
+package JavaAdvanced2021.JavaAdvanced.GenericsExercises1110;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class GenericSwapMethodString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<String> box = new Box<>();
+
+        for (int i = 1; i <= n; i++) {
+            String input = scanner.nextLine();
+            box.add(input);
+        }
+        String[] index = scanner.nextLine().split("\\s+");
+        int firstIndex = Integer.parseInt(index[0]);
+        int secondIndex = Integer.parseInt(index[1]);
+        box.swap(firstIndex, secondIndex);
+        System.out.println(box);
+    }
+
+    public static class Box<T> {
+        List<T> list;
+
+        public Box() {
+            this.list = new ArrayList<>();
+        }
+
+        public void add(T elements) {
+            this.list.add(elements);
+        }
+
+        public void swap(int firstIndex, int secondIndex) {
+            T temp = list.get(firstIndex);
+            list.set(firstIndex, list.get(secondIndex));
+            list.set(secondIndex, temp);
+            //Collections.swap(list, firstIndex, secondIndex);
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder output = new StringBuilder();
+            for (T result : list) {
+                output.append(String.format("%s: %s%n", result.getClass().getName(), result));
+            }
+            return output.toString();
+        }
+    }
+}
